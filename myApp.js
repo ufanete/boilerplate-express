0ec59dcc5da8162d27cc9b34d83c864ccc4338f9
ufanete <61036759+ufanete@users.noreply.github.com>
2023-07-26 14:30:19 -0400

    Server html file

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
+
+absolutePath = __dirname + '/views/index.html'
+
 app.get("/", function(req, res) {
-  res.send('Hello Express');
+    res.sendFile(path);
+ // res.send('Hello Express');
 });
 
 
